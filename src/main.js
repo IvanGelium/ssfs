8387f18d20d800a7dbaa3d0d07efd7bb493d1c46
Leonid Shkurchenko <l.shkurchenko@dcrt.it>
2025-12-16 21:37:16 +0300

    Fix: траблшутинг нетлифай

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './assets/main.css'
 
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 import { createApp } from 'vue'
 import Vueform from '@vueform/vueform'
 import vueformConfig from './../vueform.config'
@@ -9,6 +9,6 @@ import vueformConfig from './../vueform.config'
 const app = createApp(App)
 app.use(Vueform, vueformConfig)
 
-app.use(router)
+// app.use(router)
 
 app.mount('#app')
